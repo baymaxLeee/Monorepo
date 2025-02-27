c874388bb87569f46486d53d48a7f2460255d7e1
baymax <2321951945@qq.com>
2025-02-27 20:08:12 +0800

    revert

---
+++ packages/app-home/dist/958-f605d03e-bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkapp_home=self.webpackChunkapp_home||[]).push([[958],{958:(e,n,p)=>{p.r(n),p.d(n,{default:()=>l});var a=p(845),t=p.n(a);const l=function(){return t().createElement("div",null,"app",t().createElement("h2",null,"app-home"))}}}]);
